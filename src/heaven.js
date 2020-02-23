68c063e73ac037243a3a200157b6744c07399b8f
Sacha Wilt <sacha.wilt@gmail.com>
2020-02-24 00:23:23 +0100

    feat: reimplemented everything with promises

--- src/heaven.js
+++ src/heaven.js
@@ -1,72 +1,139 @@
+// let id = (x) => x;
+// let isPromise = (obj) => (Promise.resolve(obj) == obj)
 
-let id = (x) => x;
-
-let promise = async (fn, [err, data]) => {
-  if (err !== null) return [err, data];
-
-  return fn(data)
-    .then (data => [null, data])
-    .catch(err  => [err, null]);
-}
+const F = require("./utils");
 
 module.exports = {
+  // data => P(errdata)
+  from: (data) => Promise.resolve([null, data]),
+
+  // fn: (data->promise)
+  promise: (fn, deferredErrdata) => {
+    return new Promise(resolve => {
+      deferredErrdata.then(([err, data]) => {
+        // if (err !== null) return resolve([err, null]);
+        fn(data).then(data => {
+          resolve([null, data]);
+        })/*.catch(err => {
+          resolve([err, null]);
+        })*/
+      });
+    });
+  },
+
+  // fn: (data->errdata)
+  bind: (fn, deferredErrdata) => {
+    return new Promise(resolve => {
+      deferredErrdata.then(([err, data]) => {
+        // if (err !== null) return resolve([err, null]);
+        resolve(fn(data));
+      });/*.catch(err => {
+        resolve([err, null]);
+      })*/
+    });
+  },
+
+  // fn: (data->data)
+  map: (fn, deferredErrdata) => {
+    return new Promise(resolve => {
+      deferredErrdata.then(([err, data]) => {
+        // if (err !== null) return resolve([err, null]);
+        resolve([null, fn(data)]);
+      });/*.catch(err => {
+        resolve([err, null]);
+      })*/
+    });
+  },
+
+  // fn: (data->data)
+  guard: (fn, deferredErrdata) => {
+    return new Promise(resolve => {
+      deferredErrdata.then(([err, data]) => {
+        // if (err !== null) return resolve([err, null]);
+        let err2 = fn(data);
+
+        // if (err2) {
+        //   resolve([err2, null]);
+        //   return
+        // }
+
+        resolve([null, data]);
+      });/*.catch(err => {
+        resolve([err, null]);
+      })*/
+    });
+  },
+
+  // fn: (data->[ignored])
+  tap: (fn, deferredErrdata) => {
+    return new Promise(resolve => {
+      deferredErrdata.then(([err, data]) => {
+        // if (err !== null) return resolve([err, null]);
+        resolve([null, F.tap(fn, data)]);
+      })/*.catch(err => {
+        resolve([err, null]);
+      })*/
+    });
+  },
+
   // partial application
   // fn(x,y) == p(fn, x, y)() == p(fn, x)(y) == p(fn)(x, y)
-  p: (fn, ...args) => fn.bind(null, ...args),
+  partial: F.partial,
 
-  // pipe(f, g)(x) == g(f(x)) // f applied first
-  pipe: (...fns) => args => fns.reduce((val, fn) => fn(val), args),
+  // pipe([f, 1], [g, true])(x) == g(f(1, x), true) // Note that f applied first
+  pipe: (...fns) => args => {
+    return fns.reduce((val, [fn, ...args]) => {
+      return F.partial(fn, ...args)(val);
+    }, args);
+  }
 
-  // data => errdata
-  from: (data) => [null, data],
-
-  // err => errdata
-  error: (err) => [err, null],
-
-  // fn: data -> errdata
-  bind: (fn, [err, data]) => {
-    if (err !== null) return [err, null];
-
-    return fn(data);
-  },
-
+  // // data => errdata
+  // from: (data) => [null, data],
+  //
+  // // err => errdata
+  // error: (err) => [err, null],
+  //
+  // // fn: data -> errdata
+  // bind: (fn, [err, data]) => {
+  //   if (err !== null) return [err, null];
+  //
+  //   return fn(data);
+  // },
+  //
   // fn: data -> data
-  map: (fn, [err, data]) => {
-    if (err !== null) return [err, data];
-
-    return [null, fn(data)];
-  },
-
-  // fn: data -> (return value is ignored, just side-effects)
-  tap: (fn, [err, data]) => {
-    if (err !== null) return [err, data];
-
-    fn(data);
-
-    return [null, data];
-  },
-
-  promise,
-
-  // Transform (data, cb(err, data)) into a "errdata promise"
-  callback: async (fn, [err, data]) => {
-    return promise((data) => {
-      return new Promise((resolve, reject) => {
-        fn(data, (err, data) => {
-          if (err !== null) reject(err);
-          resolve(data);
-        });
-      })
-    }, [err, data]);
-
-    // is equivalent to (with promise dependency)
-
-    // if (err != null) return [err, null];
-    // return new Promise((resolve, reject) => {
-    //   fn(data, (err, data) => {
-    //     if (err !== null) reject([err, null]);
-    //     resolve([null, data]);
-    //   });
-    // }).then(id).catch(id);
-  },
+  // map: (fn, [err, data]) => {
+  //   if (err !== null) return [err, data];
+  //
+  //   return Promise.resolve([null, fn(data)]);
+  // },
+  //
+  // // fn: data -> (return value is ignored, just side-effects)
+  // tap: (fn, [err, data]) => {
+  //   if (err !== null) return [err, data];
+  //
+  //   fn(data);
+  //
+  //   return [null, data];
+  // },
+  // // Transform (data, cb(err, data)) into a "errdata promise"
+  // callback: async (fn, [err, data]) => {
+  //   return promise((data) => {
+  //     return new Promise((resolve, reject) => {
+  //       fn(data, (err, data) => {
+  //         if (err !== null) reject(err);
+  //         resolve(data);
+  //       });
+  //     })
+  //   }, [err, data]);
+  //
+  //   // is equivalent to (with promise dependency)
+  //
+  //   // if (err != null) return [err, null];
+  //   // return new Promise((resolve, reject) => {
+  //   //   fn(data, (err, data) => {
+  //   //     if (err !== null) reject([err, null]);
+  //   //     resolve([null, data]);
+  //   //   });
+  //   // }).then(id).catch(id);
+  // },
 }
--- tests/heaven.test.js
+++ tests/heaven.test.js
@@ -1,134 +1,159 @@
 const H = require("../src/heaven");
 
-describe("partial application (p)", () => {
-  let sum = (x, y) => (x + y);
-
-  test("applies partially arguments", () => {
-    expect(sum(1, 2))       .toEqual(3);
-    expect(H.p(sum)(1, 2))  .toEqual(3);
-    expect(H.p(sum, 1)(2))  .toEqual(3);
-    expect(H.p(sum, 1, 2)()).toEqual(3);
-  });
-});
-
-describe("pipe", () => {
-  test("applies multiple functions in order to a value", () => {
-    expect(H.pipe(x => x * 2, x => x + 1)(2)).toEqual(5);
-  });
-});
-
+// Testing promise values not available at start
+let later = (value) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, 100);
+  })
+}
 
 describe("from", () => {
-  test("convert data into errdata", () => {
-    expect(H.from("hello")).toEqual([null, "hello"]);
-  });
-});
-
-describe("error", () => {
-  test("convert err into errdata", () => {
-    expect(H.error("something bad happened")).toEqual(["something bad happened", null]);
-  });
-});
-
-describe("bind", () => {
-  let checkAdult = (user) => {
-    if (user.age < 18) {
-      return ["User should be an adult", null];
-    }
-
-    return [null, user];
-  };
-
-  test("uses fn on data when there's no error", () => {
-    expect(H.bind(checkAdult, [null, {age: 30}])).toEqual([null, {age: 30}]);
-  });
-
-  test("uses fn on data when there's no error", () => {
-    expect(H.bind(checkAdult, [null, {age: 16}])).toEqual(["User should be an adult", null]);
-  });
-
-  test("does nothing with errors", () => {
-    expect(H.bind(checkAdult, ["Invalid user name", null])).toEqual(["Invalid user name", null]);
-  });
-});
-
-describe("map", () => {
-  test("transforms data without errors", () => {
-    expect(H.map(x => (x + 1), [null, 5])).toEqual([null, 6]);
-  });
-
-  test("does nothing with errors", () => {
-    expect(H.map(x => (x + 1), ["Something went wrong", null])).toEqual(["Something went wrong", null]);
-  });
-});
-
-describe("tap", () => {
-  test("calls cb(value) and returns [null, value]", () => {
-    let cb = jest.fn().mockImplementation(() => "unusedReturn");
-
-    expect(H.tap(cb, [null, "value"])).toEqual([null, "value"]);
-    expect(cb).toHaveBeenCalledTimes(1);
-  });
-
-  test("does nothing with errors", () => {
-    let cb = jest.fn();
-
-    expect(H.tap(cb, ["error", null])).toEqual(["error", null]);
-    expect(cb).toHaveBeenCalledTimes(0);
+  test("convert data into P(errdata)", async () => {
+    expect(await H.from("hello")).toEqual([null, "hello"]);
   });
 });
 
 describe("promise", () => {
-  test("takes a data->promise fn and return [null, value] with .then()", async () => {
-    let slowInc = (x) => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(x + 1), 100);
-      });
-    };
-
-    expect(await H.promise(slowInc, [null, 2])).toEqual([null, 3]);
-  });
-
-  test("takes a promise and return [err, null] with .catch()", async () => {
-    let badInc = (x) => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => reject("too hard"), 100);
-      });
-    };
-
-    expect(await H.promise(badInc, [null, 3])).toEqual(["too hard", null]);
-  });
-
-  test("does nothing with errors", async () => {
-    let p = jest.fn();
-
-    expect(await H.promise(p, ["error", null])).toEqual(["error", null]);
-
-    expect(p).toHaveBeenCalledTimes(0);
-  });
-});
-
-describe("callback", () => {
-  let readFile = (path, callback) => {
-    if (path == "path/to/file") {
-      callback(null, "contents");
-      return;
-    }
-
-    callback("file not found");
+  let slowInc = (x) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(x + 1), 100);
+    });
   };
 
-  test("takes a data,cb->(nothing) fn and return [null, value] with cb(null, value)", async () => {
-    expect(await H.callback(readFile, [null, "path/to/file"])).toEqual([null, "contents"]);
+  test("data->promise", async () => {
+    expect(await H.promise(slowInc, later([null, 2]))).toEqual([null, 3]);
   });
 
-  test("takes a data,cb->(nothing) fn and return [err, null] with cb(err, null)", async () => {
-    expect(await H.callback(readFile, [null, "unknown"])).toEqual(["file not found", null]);
+  // @TODO: promise fails
+  // @TODO: errdata fails
+  // @TODO: err not null
+});
+
+describe("map", () => {
+  test("data->data into P(errdata)", async () => {
+    expect(await H.map(x => (x + 1), later([null, 5]))).toEqual([null, 6]);
   });
 
-  test("does nothing with errors", async () => {
-    let fakeReadFile = jest.fn();
-    expect(await H.callback(readFile, ["error", null])).toEqual(["error", null]);
-    expect(fakeReadFile).toHaveBeenCalledTimes(0);
+  // @TODO: promise fails
+  // @TODO: errdata not deferred
+  // @TODO: errdata fails
+  // @TODO: err not null
+});
+
+describe("bind", () => {
+  test("data->errdata into P(errdata)", async () => {
+    expect(await H.bind(x => [null, x], later([null, 5]))).toEqual([null, 5]);
+  });
+
+  // @TODO: promise fails
+  // @TODO: errdata not deferred
+  // @TODO: errdata fails
+  // @TODO: err not null
+});
+
+describe("guard", () => {
+  test("data->err into P(errdata)", async () => {
+    expect(await H.guard(x => null, later([null, 5]))).toEqual([null, 5]);
+  });
+
+  // @TODO: promise fails
+  // @TODO: errdata not deferred
+  // @TODO: errdata fails
+  // @TODO: err not null
+});
+
+// describe.skip("bind", () => {
+//   let checkAdult = (user) => {
+//     if (user.age < 18) {
+//       return ["User should be an adult", null];
+//     }
+//
+//     return [null, user];
+//   };
+//
+//   test("uses fn on data when there's no error", () => {
+//     expect(H.bind(checkAdult, [null, {age: 30}])).toEqual([null, {age: 30}]);
+//   });
+//
+//   test("uses fn on data when there's no error", () => {
+//     expect(H.bind(checkAdult, [null, {age: 16}])).toEqual(["User should be an adult", null]);
+//   });
+//
+//   test("does nothing with errors", () => {
+//     expect(H.bind(checkAdult, ["Invalid user name", null])).toEqual(["Invalid user name", null]);
+//   });
+// });
+
+describe("tap", () => {
+  test("data->(nothing)", async () => {
+    let cb = jest.fn().mockImplementation(() => "unusedReturn");
+
+    expect(await H.tap(cb, later([null, "value"]))).toEqual([null, "value"]);
+
+    expect(cb.mock.calls.length).toBe(1);
+    expect(cb.mock.calls[0][0]).toEqual("value");
+  });
+
+  // @TODO: promise fails
+  // @TODO: errdata not deferred
+  // @TODO: errdata fails
+  // @TODO: err not null
+});
+
+describe("partial", () => {
+  let sum = (x, y) => (x + y);
+
+  test("applies partially arguments", () => {
+    expect(sum(1, 2)).toEqual(3);
+
+    expect(H.partial(sum)(1, 2))  .toEqual(3);
+    expect(H.partial(sum, 1)(2))  .toEqual(3);
+    expect(H.partial(sum, 1, 2)()).toEqual(3);
   });
 });
+
+describe("pipe", () => {
+  let add = (x, y) => (x + y);
+  let mul = (x, y) => (x * y);
+
+  test("applies partially arguments", () => {
+    expect(H.pipe(
+      [add, 1],
+      [mul, 2],
+      [add, 2],
+    )(1)).toEqual(6);
+  });
+});
+
+// describe.skip("error", () => {
+//   test("convert err into errdata", () => {
+//     expect(H.error("something bad happened")).toEqual(["something bad happened", null]);
+//   });
+// });
+
+// describe.skip("callback", () => {
+//   let readFile = (path, callback) => {
+//     if (path == "path/to/file") {
+//       callback(null, "contents");
+//       return;
+//     }
+//
+//     callback("file not found");
+//   };
+//
+//   test("takes a data,cb->(nothing) fn and return [null, value] with cb(null, value)", async () => {
+//     expect(await H.callback(readFile, [null, "path/to/file"])).toEqual([null, "contents"]);
+//   });
+//
+//   test("takes a data,cb->(nothing) fn and return [err, null] with cb(err, null)", async () => {
+//     expect(await H.callback(readFile, [null, "unknown"])).toEqual(["file not found", null]);
+//   });
+//
+//   test("does nothing with errors", async () => {
+//     let fakeReadFile = jest.fn();
+//     expect(await H.callback(readFile, ["error", null])).toEqual(["error", null]);
+//     expect(fakeReadFile).toHaveBeenCalledTimes(0);
+//   });
+// });
